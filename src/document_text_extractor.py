080deea09f733cf83ca1fe6732c1a96ac634f823
Lalit Suthar <lalitsuthar4444@gmail.com>
2026-04-23 15:22:31 +0530

    batch wise text extracting

--- src/document_text_extractor.py
+++ src/document_text_extractor.py
@@ -1,9 +1,15 @@
 from docling.document_converter import DocumentConverter
+import fitz
 
 def extract_text(file_path):
+    doc = fitz.open(file_path)
+    total_pages = len(doc)
     converter = DocumentConverter()
-    result = converter.convert(file_path)
-    text = result.document.export_to_markdown()
+    text=""
+    for i in range(1,total_pages,5):
+        result = converter.convert(file_path,page_range=(i,i+5))
+        text += result.document.export_to_markdown()
+        print(f"Processing pages {i} to {i+5} of {total_pages}")
     
     return text
 
